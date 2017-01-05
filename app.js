626c34eda4a59ebe4c1a5af91ba844ad0ca71422
PatTaylor <patrick.taylor31@gmail.com>
2017-01-05 15:33:13 +0100

    Commit Final

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('have fun !');
+$("input").keypress(function(x) {
+    var firstname = $("#first_name").val();
+    var lastname = $("#last_name").val();
+    var city = $("#city").val();
+    if (x.which == 13) {
+        console.log("I'm" + " " + firstname + " " + lastname + " from " + city);
+        $("#username").replaceWith(firstname + " " + lastname);
+    }
+
+});
